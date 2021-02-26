701817eb3de07b8602ca869e005fc3b24d96f9a8
Juan Martin Ludueña <juanmluduena1@gmail.com>
2021-02-26 11:02:02 -0300

    Creado el microservicio de usuarios

---
+++ springboot-servicio-usuarios/src/main/java/com/springboot/app/users/SpringbootServicioUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioUsuariosApplication.class, args);
+	}
+
+}
