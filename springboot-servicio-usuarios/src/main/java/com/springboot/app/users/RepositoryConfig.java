8f3f82f8594d24d06a6d891f9e8ac6dc298b8953
Juan Martin Ludueña <juanmluduena1@gmail.com>
2021-02-26 12:29:03 -0300

    Configurando mostrar ID en requests

---
+++ springboot-servicio-usuarios/src/main/java/com/springboot/app/users/RepositoryConfig.java
@@ -0,0 +1,18 @@
+package com.springboot.app.users;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import com.springboot.app.users.models.entity.Role;
+import com.springboot.app.users.models.entity.Usuario;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Usuario.class, Role.class);
+	}
+
+}
--- springboot-servicio-usuarios/src/main/java/com/springboot/app/users/models/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/springboot/app/users/models/dao/UsuarioDao.java
@@ -2,7 +2,9 @@ package com.springboot.app.users.models.dao;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.springboot.app.users.models.entity.Usuario;
 
@@ -10,7 +12,8 @@ import com.springboot.app.users.models.entity.Usuario;
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
 
 	
-	public Usuario findByUsername(String username);
+	@RestResource(path="buscar-username")
+	public Usuario findByUsername(@Param("username")String username);
 	
 	@Query("select u from Usuario u where u.username = ?1")
 	public Usuario obtenerPorUsername(String username);
